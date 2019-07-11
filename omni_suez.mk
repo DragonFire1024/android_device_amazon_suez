e42f5243a0c598f80e7f37aa45259b15f6373c7d
Jason Safoutin <DragonFire1024@gmail.com>
2019-07-11 10:44:51 -0400

    Delete omni_suez.mk

--- omni_suez.mk
+++
@@ -1,9 +0,0 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
-$(call inherit-product, device/amazon/suez/device.mk)
-
-
-PRODUCT_NAME := omni_suez
-PRODUCT_DEVICE := suez
-PRODUCT_BRAND := Android
-PRODUCT_MANUFACTURER := Amazon
